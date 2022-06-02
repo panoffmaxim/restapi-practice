5b0a32aa7337b4120cc89b20afa259dc354b89a8
Funiq <panoffmaxim@gmail.com>
2022-06-02 09:44:27 +0600

    Added Spring Security support (fixed url entries)

--- src/main/java/com/epam/laboratory/restapipractice/config/WebSecurityConfig.java
+++ src/main/java/com/epam/laboratory/restapipractice/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.epam.laboratory.restapipractice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,10 +15,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/clients/random");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/clients**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()

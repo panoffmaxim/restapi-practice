0a2526c4a74935ff4db57c342b0a93e59bc572c5
Funiq <panoffmaxim@gmail.com>
2022-07-21 13:15:56 +0600

    Deleted Spring Data JPA support and added Hibernate support (some commit fixes ver.2)

--- src/main/java/com/epam/laboratory/restapipractice/config/WebSecurityConfig.java
+++ src/main/java/com/epam/laboratory/restapipractice/config/WebSecurityConfig.java
@@ -1,75 +1,75 @@
-package com.epam.laboratory.restapipractice.config;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-
-@Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true
-)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private DataSource dataSource;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/clients/random").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
-    }
-
-    /*@Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().passwordEncoder(encoder())
-                .dataSource(dataSource);
-                //.usersByUsernameQuery("select * from client where clientName=?")
-    //            .authoritiesByUsernameQuery("select c.clientName, cr.roles from client c inner join client_role cr on c.id = cr.client_id where c.clientName=?");
-    }*/
-
-    @Bean
-    public RoleHierarchy roleHierarchy() {
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        String hierarchy = "ROLE_ADMIN > ROLE_STAFF \n ROLE_STAFF > ROLE_USER";
-        roleHierarchy.setHierarchy(hierarchy);
-        return roleHierarchy;
-    }
-
-    @Override
-    public void configure(WebSecurity webSecurity) {
-        webSecurity.expressionHandler(webSecurityExpressionHandler());
-    }
-
-    @Bean
-    public DefaultWebSecurityExpressionHandler webSecurityExpressionHandler() {
-        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
-        expressionHandler.setRoleHierarchy(roleHierarchy());
-        return expressionHandler;
-    }
-
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder(11);
-    }
-}
+//package com.epam.laboratory.restapipractice.config;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+//import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(
+//        prePostEnabled = true
+//)
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/clients/random").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+//    }
+//
+//    /*@Autowired
+//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication().passwordEncoder(encoder())
+//                .dataSource(dataSource);
+//                //.usersByUsernameQuery("select * from client where clientName=?")
+//    //            .authoritiesByUsernameQuery("select c.clientName, cr.roles from client c inner join client_role cr on c.id = cr.client_id where c.clientName=?");
+//    }*/
+//
+//    @Bean
+//    public RoleHierarchy roleHierarchy() {
+//        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+//        String hierarchy = "ROLE_ADMIN > ROLE_STAFF \n ROLE_STAFF > ROLE_USER";
+//        roleHierarchy.setHierarchy(hierarchy);
+//        return roleHierarchy;
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity webSecurity) {
+//        webSecurity.expressionHandler(webSecurityExpressionHandler());
+//    }
+//
+//    @Bean
+//    public DefaultWebSecurityExpressionHandler webSecurityExpressionHandler() {
+//        DefaultWebSecurityExpressionHandler expressionHandler = new DefaultWebSecurityExpressionHandler();
+//        expressionHandler.setRoleHierarchy(roleHierarchy());
+//        return expressionHandler;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder encoder() {
+//        return new BCryptPasswordEncoder(11);
+//    }
+//}
--- src/main/java/com/epam/laboratory/restapipractice/security/MyUserDetailsService.java
+++ src/main/java/com/epam/laboratory/restapipractice/security/MyUserDetailsService.java
@@ -1,74 +1,74 @@
-package com.epam.laboratory.restapipractice.security;
-
-import com.epam.laboratory.restapipractice.entity.ClientEntity;
-import com.epam.laboratory.restapipractice.entity.Privilege;
-import com.epam.laboratory.restapipractice.entity.Role;
-import com.epam.laboratory.restapipractice.repository.ClientRepo;
-import com.epam.laboratory.restapipractice.repository.RoleRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-@Service("userDetailsService")
-@Transactional
-public class MyUserDetailsService implements UserDetailsService {
-    @Autowired
-    private ClientRepo clientRepo;
-    @Autowired
-    private RoleRepo roleRepo;
-
-    @Override
-    public UserDetails loadUserByUsername(String clientName)
-            throws UsernameNotFoundException {
-
-        ClientEntity clientEntity = clientRepo.findByClientName(clientName);
-        if (clientEntity == null) {
-            return new org.springframework.security.core.userdetails.User(
-                    " ", " ", true, true, true, true,
-                    getAuthorities(Arrays.asList(
-                            roleRepo.findByName("ROLE_USER"))));
-        }
-
-        return new org.springframework.security.core.userdetails.User(
-                clientEntity.getClientName(), clientEntity.getPassword(), clientEntity.isEnabled(), true, true,
-                true, getAuthorities(clientEntity.getRoles()));
-    }
-
-    private Collection<? extends GrantedAuthority> getAuthorities(
-            Collection<Role> roles) {
-
-        return getGrantedAuthorities(getPrivileges(roles));
-    }
-
-    private List<String> getPrivileges(Collection<Role> roles) {
-        List<String> privileges = new ArrayList<>();
-        List<Privilege> collection = new ArrayList<>();
-
-        roles.stream().forEach(role -> {
-            privileges.add(role.getName());
-            collection.addAll(role.getPrivileges());
-        });
-        collection.stream().forEach(privilege -> {
-            privileges.add(privilege.getName());
-        });
-        return privileges;
-    }
-
-    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        for (String privilege : privileges) {
-            authorities.add(new SimpleGrantedAuthority(privilege));
-        }
-        return authorities;
-    }
-}
+//package com.epam.laboratory.restapipractice.security;
+//
+//import com.epam.laboratory.restapipractice.entity.ClientEntity;
+//import com.epam.laboratory.restapipractice.entity.Privilege;
+//import com.epam.laboratory.restapipractice.entity.Role;
+//import com.epam.laboratory.restapipractice.repository.ClientRepo;
+//import com.epam.laboratory.restapipractice.repository.RoleRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import javax.transaction.Transactional;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collection;
+//import java.util.List;
+//
+//@Service("userDetailsService")
+//@Transactional
+//public class MyUserDetailsService implements UserDetailsService {
+//    @Autowired
+//    private ClientRepo clientRepo;
+//    @Autowired
+//    private RoleRepo roleRepo;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String clientName)
+//            throws UsernameNotFoundException {
+//
+//        ClientEntity clientEntity = clientRepo.findByClientName(clientName);
+//        if (clientEntity == null) {
+//            return new org.springframework.security.core.userdetails.User(
+//                    " ", " ", true, true, true, true,
+//                    getAuthorities(Arrays.asList(
+//                            roleRepo.findByName("ROLE_USER"))));
+//        }
+//
+//        return new org.springframework.security.core.userdetails.User(
+//                clientEntity.getClientName(), clientEntity.getPassword(), clientEntity.isEnabled(), true, true,
+//                true, getAuthorities(clientEntity.getRoles()));
+//    }
+//
+//    private Collection<? extends GrantedAuthority> getAuthorities(
+//            Collection<Role> roles) {
+//
+//        return getGrantedAuthorities(getPrivileges(roles));
+//    }
+//
+//    private List<String> getPrivileges(Collection<Role> roles) {
+//        List<String> privileges = new ArrayList<>();
+//        List<Privilege> collection = new ArrayList<>();
+//
+//        roles.stream().forEach(role -> {
+//            privileges.add(role.getName());
+//            collection.addAll(role.getPrivileges());
+//        });
+//        collection.stream().forEach(privilege -> {
+//            privileges.add(privilege.getName());
+//        });
+//        return privileges;
+//    }
+//
+//    private List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+//        List<GrantedAuthority> authorities = new ArrayList<>();
+//        for (String privilege : privileges) {
+//            authorities.add(new SimpleGrantedAuthority(privilege));
+//        }
+//        return authorities;
+//    }
+//}

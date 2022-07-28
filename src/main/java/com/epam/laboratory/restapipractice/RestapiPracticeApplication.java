1e51cbdf173da5f1be9aa4f1874bae5c10439815
panoffmaxim <71684725+panoffmaxim@users.noreply.github.com>
2022-07-29 01:22:56 +0600

    Deleted Spring Data JPA support and added Hibernate support (deleted EntryPoint.java)

--- src/main/java/com/epam/laboratory/restapipractice/RestapiPracticeApplication.java
+++ src/main/java/com/epam/laboratory/restapipractice/RestapiPracticeApplication.java
@@ -2,8 +2,10 @@ package com.epam.laboratory.restapipractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.epam.laboratory.restapipractice.repository")
 public class RestapiPracticeApplication {
 
 	public static void main(String[] args) {

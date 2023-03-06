9d42a923d43bed51d8f0b165b3e3e88e500f0ff7
panoffmaxim <panoffmaxim@gmail.com>
2023-03-06 23:47:34 +0600

    Minor test fixes ver.2

--- src/test/java/com/epam/laboratory/restapipractice/controller/OrderControllerTest.java
+++ src/test/java/com/epam/laboratory/restapipractice/controller/OrderControllerTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class OrderContollerTest {
+public class OrderControllerTest {
 
     @Mock
     private OrderService orderService;
@@ -23,8 +23,9 @@ public class OrderContollerTest {
     @Test
     void shouldReturnNotFoundIfOrderServiceReturnNull() {
 //        OrderController orderController = new OrderController(orderService);
-        when(orderService.getAllOrders()).thenReturn(null);
+        Mockito.when(orderService.getAllOrders()).thenReturn(null);
         var response = orderController.getAllOrders();
+        Mockito.verify(orderController.getAllOrders());
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }

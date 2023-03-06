f4a312e1aed01f774faca15d1ada419485b88e11
panoffmaxim <panoffmaxim@gmail.com>
2023-03-06 18:50:40 +0600

    Minor test fixes

--- src/test/java/com/epam/laboratory/restapipractice/controller/OrderContollerTest.java
+++ src/test/java/com/epam/laboratory/restapipractice/controller/OrderContollerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,8 +25,6 @@ public class OrderContollerTest {
 //        OrderController orderController = new OrderController(orderService);
         when(orderService.getAllOrders()).thenReturn(null);
         var response = orderController.getAllOrders();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
-
-    //spring mvc почитать
-    //кэширование
 }

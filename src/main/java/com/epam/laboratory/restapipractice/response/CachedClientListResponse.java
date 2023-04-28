85e59466cb5a14aaf9537afb33dfb3cd9598a47d
panoffmaxim <panoffmaxim@gmail.com>
2023-04-28 16:43:14 +0600

    Added default constructors and setters in CachedClientResponse and CachedClientListResponse

--- src/main/java/com/epam/laboratory/restapipractice/response/CachedClientListResponse.java
+++ src/main/java/com/epam/laboratory/restapipractice/response/CachedClientListResponse.java
@@ -6,22 +6,29 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CachedClientListResponse {
-    private final List<CachedClientResponse> cashedClientResponseList;
+    private List<CachedClientResponse> cashedClientResponseList;
 
-    public CachedClientListResponse(final List<CachedClientResponse> cashedClientResponseList) {
+    public CachedClientListResponse() {
+    }
+
+    public CachedClientListResponse(List<CachedClientResponse> cashedClientResponseList) {
         this.cashedClientResponseList = cashedClientResponseList;
     }
 
-    public List<CachedClientResponse> getCashedClients() {
+    public List<CachedClientResponse> getCashedClientResponseList() {
         return cashedClientResponseList;
+    }
+
+    public void setCashedClientResponseList(List<CachedClientResponse> cashedClientResponseList) {
+        this.cashedClientResponseList = cashedClientResponseList;
     }
 
     public static CachedClientListResponse fromEntityListToCachedList(List<ClientEntity> clientEntityList) {
         List<CachedClientResponse> cachedList = clientEntityList.stream().map(clientEntity -> new CachedClientResponse(clientEntity.getId(),
-                clientEntity.getClientName(), clientEntity.getOrders().stream()
-                .map(orderEntity -> new CachedClientResponse.CachedClientOrderResponse(orderEntity.getId(),
-                        orderEntity.getCompleted(),
-                        orderEntity.getDeliveryInf()))
+                        clientEntity.getClientName(), clientEntity.getOrders().stream()
+                        .map(orderEntity -> new CachedClientResponse.CachedClientOrderResponse(orderEntity.getId(),
+                                orderEntity.getCompleted(),
+                                orderEntity.getDeliveryInf()))
                         .collect(Collectors.toList())))
                 .collect(Collectors.toList());
         return new CachedClientListResponse(cachedList);

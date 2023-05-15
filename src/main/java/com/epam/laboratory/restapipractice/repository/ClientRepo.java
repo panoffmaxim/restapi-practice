173cd8f1e6fce1a73025f48036f6af054e1287f1
panoffmaxim <panoffmaxim@gmail.com>
2023-05-15 13:59:16 +0600

    Added Spring Data Jpa support

--- src/main/java/com/epam/laboratory/restapipractice/repository/ClientRepo.java
+++ src/main/java/com/epam/laboratory/restapipractice/repository/ClientRepo.java
@@ -2,15 +2,17 @@ package com.epam.laboratory.restapipractice.repository;
 
 import com.epam.laboratory.restapipractice.customannotations.ClientBean;
 import com.epam.laboratory.restapipractice.entity.ClientEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 @ClientBean
-public interface ClientRepo {
-    ClientEntity saveClient(ClientEntity client);
-    Boolean updateClient(ClientEntity client);
-    Boolean deleteClientById(Long id);
-    ClientEntity findClientById(Long id);
-    List<ClientEntity> findAllClients();
+public interface ClientRepo extends JpaRepository<ClientEntity, Long> {
+    //    ClientEntity saveClient(ClientEntity client);
+//    Boolean updateClient(ClientEntity client);
+//    Boolean deleteClientById(Long id);
+//    ClientEntity findClientById(Long id);
+//    List<ClientEntity> findAllClients();
     ClientEntity findByClientName(String clientName);
 }
--- src/main/java/com/epam/laboratory/restapipractice/repository/OrderRepo.java
+++ src/main/java/com/epam/laboratory/restapipractice/repository/OrderRepo.java
@@ -1,11 +1,14 @@
 package com.epam.laboratory.restapipractice.repository;
 
+import com.epam.laboratory.restapipractice.entity.ClientEntity;
 import com.epam.laboratory.restapipractice.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface OrderRepo {
-    OrderEntity saveOrder(OrderEntity order);
-    OrderEntity findOrderById(Long id);
-    List<OrderEntity> findAllOrders();
+public interface OrderRepo extends JpaRepository<OrderEntity, Long> {
+//    OrderEntity saveOrder(OrderEntity order);
+//    OrderEntity findOrderById(Long id);
+//    List<OrderEntity> findAllOrders();
 }

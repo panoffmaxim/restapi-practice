bc5537d8c35881efb48226e0d45c37c2d70777fb
panoffmaxim <panoffmaxim@gmail.com>
2023-10-30 18:52:03 +0600

    Reworked documenting REST API using OpenAPI 3.0

--- src/main/java/com/epam/laboratory/restapipractice/dto/ClientResponseDto.java
+++ src/main/java/com/epam/laboratory/restapipractice/dto/ClientResponseDto.java
@@ -1,8 +1,13 @@
 package com.epam.laboratory.restapipractice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class ClientResponseDto {
+    @Schema(description = "Уникальный идентификатор клиента")
     private Long id;
+    @Schema(description = "Имя клиента")
     private String clientName;
+    @Schema(description = "Номер телефона клиента")
     private String phone;
 
     public ClientResponseDto() {
--- src/main/java/com/epam/laboratory/restapipractice/dto/ClientsListResponseDto.java
+++ src/main/java/com/epam/laboratory/restapipractice/dto/ClientsListResponseDto.java
@@ -1,8 +1,11 @@
 package com.epam.laboratory.restapipractice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 
 public class ClientsListResponseDto {
+    @Schema(description = "Возвращает список клиентов")
     private final List<ClientResponseDto> clients;
 
     public ClientsListResponseDto(final List<ClientResponseDto> clients) {

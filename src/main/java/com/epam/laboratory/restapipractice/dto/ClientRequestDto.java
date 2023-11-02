8546b97c83d0fd7f5d1e4c8b40808d0c379802f4
panoffmaxim <71684725+panoffmaxim@users.noreply.github.com>
2023-11-02 10:03:22 +0600

    OpenAPI (#8)
    Added documenting REST API using OpenAPI 3.0

--- src/main/java/com/epam/laboratory/restapipractice/dto/ClientRequestDto.java
+++ src/main/java/com/epam/laboratory/restapipractice/dto/ClientRequestDto.java
@@ -1,7 +1,11 @@
 package com.epam.laboratory.restapipractice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class ClientRequestDto {
+    @Schema(description = "Имя клиента", example = "Иван")
     private String clientName;
+    @Schema(description = "Телефон клиента", example = "+7-700-111-11-11")
     private String phone;
 
     public ClientRequestDto() {
--- src/main/java/com/epam/laboratory/restapipractice/response/RandomResponse.java
+++ src/main/java/com/epam/laboratory/restapipractice/response/RandomResponse.java
@@ -1,7 +1,11 @@
 package com.epam.laboratory.restapipractice.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public class RandomResponse {
+    @Schema(description = "Название сервиса")
     private final String service;
+    @Schema(description = "Случайный факт")
     private final String randomFact;
 
     public RandomResponse(String service, String randomFact) {

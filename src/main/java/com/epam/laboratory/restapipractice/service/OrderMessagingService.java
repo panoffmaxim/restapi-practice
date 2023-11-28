5e80e8dfcda8b779cc0e8ebfc8df99c38f34fdac
panoffmaxim <panoffmaxim@gmail.com>
2023-11-28 16:30:16 +0600

    Added some fixes

--- src/main/java/com/epam/laboratory/restapipractice/service/OrderMessagingService.java
+++ src/main/java/com/epam/laboratory/restapipractice/service/OrderMessagingService.java
@@ -12,13 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderMessagingService {
 
     private final OrderService orderService;
-    private static final String topicName = "${message.topic.name}";
-    private static final String kafkaConsumerGroupId = "${spring.kafka.consumer.group-id}";
+    private static final String TOPIC_NAME = "${message.topic.name}";
+    private static final String KAFKA_CONSUMER_GROUP_ID = "${spring.kafka.consumer.group-id}";
 
     @Transactional
-    @KafkaListener(topics = topicName, groupId = kafkaConsumerGroupId)
+    @KafkaListener(topics = TOPIC_NAME, groupId = KAFKA_CONSUMER_GROUP_ID)
     public void printOrder(String message) {
         log.info("Message consumed {}", message);
-        orderService.completeOrder(Long.valueOf(message).longValue());
+        orderService.completeOrder(Long.parseLong(message));
     }
 }
